8b403c57b00c1c4b7c9fd53d534424b42ed5f0cd
DC <dcposch@dcpos.ch>
2014-12-26 03:30:40 -0800

    Scramble atom-shell prototype

---
+++ main.js
@@ -0,0 +1,14 @@
+var App = require("app");
+var BrowserWindow = require("browser-window");
+
+var mainWindow = null;
+
+// Quit when all windows are closed, even on Darwin
+App.on("window-all-closed", App.quit.bind(App));
+
+// Wait for atom-shell to initialize
+App.on("ready", function() {
+  mainWindow = new BrowserWindow({"width":1000,"height":700});
+  mainWindow.loadUrl("file://" + __dirname + "/index.html");
+});
+
