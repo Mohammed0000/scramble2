8ab0e2e2a701dee8f7db22e58320c1ee48c02817
DC <dcposch@dcpos.ch>
2015-01-17 23:59:44 -0800

    Remove Browserify, atom has its own packager
    Also remove react-addons, no longer needed

--- jsx/App.jsx
+++ jsx/App.jsx
@@ -1,12 +1,7 @@
 var React = require("react");
 var Login = require("./Login.jsx");
 var Inbox = require("./Inbox.jsx");
-var ScrambleMailRepo = require("scramble-mail-repo");
 
 var inbox = (<Inbox />);
-var mailRepo = new ScrambleMailRepo("~/scramble-test-dir");
-mailRepo.search("hello", function(err, msgs){
-  inbox.setState("messages", msgs);
-});
 React.render(inbox, document.body);
 
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var Keybase = require("node-keybase");
 var App = require("app");
 var BrowserWindow = require("browser-window");
+//TODO: difficult because MailRepo relies on native modules for sqlite3,
+// and atom-shell runs a different version of V8 than standard Node: https://github.com/atom/atom-shell/issues/533
+//var ScrambleMailRepo = require("scramble-mail-repo"); 
 
 var mainWindow = null;
 
@@ -9,23 +12,42 @@ App.on("window-all-closed", App.quit.bind(App));
 
 // Wait for atom-shell to initialize
 App.on("ready", function() {
+  console.log(process.version);
+  console.log(process);
   mainWindow = new BrowserWindow({"width":1000,"height":700});
   mainWindow.loadUrl("file://" + __dirname + "/build/index.html");
   mainWindow.openDevTools();
+
+  demoMailRepo();
+  demoKeybase();
 });
 
-var keybaseUser = process.argv[2];
-var keybasePassphrase = process.argv[3];
-if(!keybaseUser || !keybasePassphrase){
-  console.log("To test Keybase: node <...> <keybase user> <keybase passphrase>");
-} else {
-  var keybase = new Keybase(keybaseUser, keybasePassphrase);
-  keybase.login(function(err, result){
-    if(err){
-      console.log("Keybase login error", err);
-      return;
-    }
-    console.log("Keybase login: ", result.status.name);
+
+
+// Demo: searchable mail repo
+function demoMailRepo() {
+  var mailRepo = new ScrambleMailRepo("~/scramble-test-dir");
+  mailRepo.search("hello", function(err, msgs){
+    //inbox.setState("messages", msgs);
+    mailWindow.webContents.send("test", msgs);
   });
+};
+
+// Demo: Keybase
+function demoKeybase() {
+  var keybaseUser = process.argv[2];
+  var keybasePassphrase = process.argv[3];
+  if(!keybaseUser || !keybasePassphrase){
+    console.log("To test Keybase: node <...> <keybase user> <keybase passphrase>");
+  } else {
+    var keybase = new Keybase(keybaseUser, keybasePassphrase);
+    keybase.login(function(err, result){
+      if(err){
+        console.log("Keybase login error", err);
+        return;
+      }
+      console.log("Keybase login: ", result.status.name);
+    });
+  }
 }
 
